76486e0af1f3ba637a85efe92efd396af38fa3e7
Sebi <SebastianStoff@gmx.at>
2019-07-08 19:41:58 +0200

    added UserInfoBox with base styling concept

--- src/Common/components/UserInfoBox/index.tsx
+++ src/Common/components/UserInfoBox/index.tsx
@@ -1,13 +1,32 @@
 import React from "react";
+import styles from "./styles.module.scss";
+import { joinClasses } from "Common/utils/joinClasses";
 
-const UserInfoBox: React.FC = props => {
+interface Props {
+  name?: string;
+  email?: string;
+  org?: string;
+}
+
+const UserInfoBox: React.FC<Props> = ({
+  name = "Please enter a name",
+  email = "Please enter an Email",
+  org = ""
+}) => {
   return (
-    <>
-      <h1>UserInfoBox component!</h1>
-      <p>
-        User XYZ like https://mariannegranum.files.wordpress.com/2011/12/newtwitterprofile.png ?
-      </p>
-    </>
+    <div className="columns">
+      <div className={joinClasses("column is-4", styles.userInfoBoxColumn)}>
+        <div className={styles.imgContainer}></div>
+        <hr></hr>
+        <div className={styles.shortInfo}>
+          <h4>Account Info</h4>
+          <p>Name: {name}</p>
+          <p>Email: {email}</p>
+          <p>Organisation: {org}</p>
+        </div>
+      </div>
+      <div className={joinClasses("column", styles.userInfoBoxColumn)}></div>
+    </div>
   );
 };
 
