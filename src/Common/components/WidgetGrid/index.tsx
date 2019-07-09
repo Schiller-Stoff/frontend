ee4d37943ab6012f265560b99f54a02142f45a2c
Sebi <SebastianStoff@gmx.at>
2019-07-09 11:10:40 +0200

    base layout  and styling

--- src/Common/components/WidgetGrid/index.tsx
+++ src/Common/components/WidgetGrid/index.tsx
@@ -1,11 +1,33 @@
 import React from "react";
+import WidgetCard from "./WidgetCard";
+import WidgetControl from "./WidgetControl";
+import styles from "./styles.module.scss";
+import { joinClasses } from "Common/utils/joinClasses";
 
 const WidgetGrid: React.FC = () => {
   return (
     <>
-      <div className="column">bla</div>
-      <div className="column">bla</div>
-      <div className="column">bla</div>
+      <div className={joinClasses("columns", styles.widgetGrid)}>
+        <div className="column is-1">
+          <WidgetControl></WidgetControl>
+        </div>
+        <div className="column">
+          <div className="columns">
+            <div className="column">
+              <WidgetCard></WidgetCard>
+            </div>
+            <div className="column">
+              <WidgetCard></WidgetCard>
+            </div>
+            <div className="column">
+              <WidgetCard></WidgetCard>
+            </div>
+            <div className="column">
+              <WidgetCard></WidgetCard>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
