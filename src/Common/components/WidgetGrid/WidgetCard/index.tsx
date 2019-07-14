0d611d84ef55fd1ad9b5b3bd9297f07ca747ffc6
Sebastian Stoff <SebastianStoff@gmx.at>
2019-07-14 10:32:53 +0200

    playing around with dragable items

--- src/Common/components/WidgetGrid/WidgetCard/index.tsx
+++ src/Common/components/WidgetGrid/WidgetCard/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./styles.module.scss";
 import { joinClasses } from "Common/utils/joinClasses";
 
@@ -14,8 +14,37 @@ const WidgetCard: React.FC<Props> = ({
   backgrColor = "",
   children = null
 }) => {
+  const [dragElem, setDragElem] = useState(null);
+
+  const dragStartHandler = (event: any) => {
+    console.log("%cdrag started", "color:blue; margin:5px; border-bottom: 2px dashed blue");
+    let target: HTMLElement | any = event.target;
+    if (target === null) return;
+
+    setDragElem(target);
+  };
+
+  const dragHandler = (event: any) => {
+    console.log("%cdragging", "color:blue; margin:5px; border-bottom: 2px dashed blue");
+    let target: HTMLElement | any = dragElem;
+    if (target === null) return;
+
+    target.style.position = "absolute";
+    target.style.left = event.clientX;
+    target.style.top = event.clientY;
+  };
+
+  const dragEndHandler = () => {
+    console.log("%cdrag ended", "color:blue; margin:5px; border-bottom: 2px dashed blue");
+  };
+
   return (
     <div
+      draggable
+      onDragStart={dragStartHandler}
+      onDragOver={dragHandler}
+      //onDrop={dragEndHandler}
+      onDragEnd={dragEndHandler}
       className={joinClasses(styles.widgetCard, ...classes, "column")}
       style={{ backgroundColor: backgrColor, visibility: visible ? "initial" : "hidden" }}
     >
