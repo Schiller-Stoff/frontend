11cfe23e01525c1312a1a8d042d01573af6f4ecf
Sebastian Stoff <SebastianStoff@gmx.at>
2019-07-16 08:25:27 +0200

    corrected order numbers

--- src/Common/components/WidgetGrid/index.tsx
+++ src/Common/components/WidgetGrid/index.tsx
@@ -14,26 +14,26 @@ const WidgetGrid: React.FC = () => {
         </div> */}
         <div className="column" style={{ width: "5000px" }}>
           <div className="columns is-multiline is-mobile">
-            <WidgetCard classes={["is-4"]}>
+            <WidgetCard classes={["is-4"]} orderNumber={0}>
               <UserInfoBox email="test" name="test" />
             </WidgetCard>
-            <WidgetCard classes={["is-8"]} visible={false} orderNumber={0}></WidgetCard>
-            <WidgetCard classes={["is-2"]} backgrColor="tomato" orderNumber={1}></WidgetCard>
-            <WidgetCard classes={["is-5"]} orderNumber={2}></WidgetCard>
-            <WidgetCard classes={["is-5"]} orderNumber={3} backgrColor="orange"></WidgetCard>
-            <WidgetCard classes={["is-3"]} orderNumber={4}></WidgetCard>
+            <WidgetCard classes={["is-8"]} visible={false} orderNumber={1}></WidgetCard>
+            <WidgetCard classes={["is-2"]} backgrColor="tomato" orderNumber={2}></WidgetCard>
+            <WidgetCard classes={["is-5"]} orderNumber={3}></WidgetCard>
+            <WidgetCard classes={["is-5"]} orderNumber={4} backgrColor="orange"></WidgetCard>
             <WidgetCard classes={["is-3"]} orderNumber={5}></WidgetCard>
             <WidgetCard classes={["is-3"]} orderNumber={6}></WidgetCard>
-            <WidgetCard classes={["is-3"]} backgrColor="lightgreen" orderNumber={7}></WidgetCard>
+            <WidgetCard classes={["is-3"]} orderNumber={7}></WidgetCard>
+            <WidgetCard classes={["is-3"]} backgrColor="lightgreen" orderNumber={8}></WidgetCard>
             <WidgetCard
               classes={["is-8"]}
               backgrColor="lightgreen"
               visible={false}
-              orderNumber={8}
+              orderNumber={9}
             ></WidgetCard>
-            <WidgetCard classes={["is-4"]} backgrColor="lightgreen" orderNumber={9}></WidgetCard>
-            <WidgetCard classes={["is-12"]} orderNumber={10}></WidgetCard>
-            <WidgetCard classes={["is-12"]} visible={false} orderNumber={11}></WidgetCard>
+            <WidgetCard classes={["is-4"]} backgrColor="lightgreen" orderNumber={10}></WidgetCard>
+            <WidgetCard classes={["is-12"]} orderNumber={11}></WidgetCard>
+            <WidgetCard classes={["is-12"]} visible={false} orderNumber={12}></WidgetCard>
           </div>
         </div>
       </div>
