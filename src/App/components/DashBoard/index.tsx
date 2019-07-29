9797a34eb51f8e6fdf844c521e675a57df864d77
Sebi <SebastianStoff@gmx.at>
2019-07-29 12:41:17 +0200

    dummy logs avoids linting issues

--- src/App/components/DashBoard/index.tsx
+++ src/App/components/DashBoard/index.tsx
@@ -1,15 +1,21 @@
 import React, { useState, useEffect } from "react";
 import WidgetGrid from "Common/components/WidgetGrid";
 import { UserResponse } from "Data/types";
+import { WidgetCardObj } from "Common/types";
+
 interface Props {
   userData?: UserResponse;
 }
 const DashBoard: React.FC<Props> = ({ userData = null }) => {
-  const [widgetArray, setWidgetCardArray] = useState(null);
+  const [widgetArray, setWidgetCardArray] = useState<Array<WidgetCardObj> | null>(null);
   console.log(widgetArray, setWidgetCardArray);
 
   useEffect(() => {
-    console.log("Dummy to to remember");
+    if (userData) {
+      let cardArray: Array<WidgetCardObj> = [];
+      console.log(cardArray);
+      setWidgetCardArray(null);
+    }
 
     // change from default to received data from Profile
   }, [userData]);
