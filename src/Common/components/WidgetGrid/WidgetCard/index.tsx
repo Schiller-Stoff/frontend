ac99648beacfcc543a8ec9cba94b2855ca4c94a0
Sebastian Stoff <SebastianStoff@gmx.at>
2019-08-05 19:42:36 +0200

    changed default bulma class value

--- src/Common/components/WidgetGrid/WidgetCard/index.tsx
+++ src/Common/components/WidgetGrid/WidgetCard/index.tsx
@@ -14,7 +14,7 @@ interface Props {
 
 const WidgetCard: React.FC<Props> = ({
   visible = true,
-  classes = [""],
+  classes = ["is-4"],
   backgrColor = "",
   children = null,
   orderNumber = null,
