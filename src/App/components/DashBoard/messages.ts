e8cacbe916935d1412c031fe7f4d1d784d8fa738
Sebastian Stoff <SebastianStoff@gmx.at>
2019-08-05 19:44:25 +0200

    copied message to the dashboard meanwhile?

---
+++ src/App/components/DashBoard/messages.ts
@@ -0,0 +1,14 @@
+import { defineMessages } from "react-intl";
+
+export const messages = defineMessages({
+  name: {
+    defaultMessage: "Name",
+    description: "The name label",
+    id: "Profile.Name"
+  },
+  email: {
+    defaultMessage: "Email",
+    description: "The email label",
+    id: "Profile.Email"
+  }
+});
