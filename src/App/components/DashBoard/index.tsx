4e89eba712d1128902e22368e458625376b73b10
Sebastian Stoff <SebastianStoff@gmx.at>
2019-08-05 19:54:47 +0200

    user card is now generated from userData

--- src/App/components/DashBoard/index.tsx
+++ src/App/components/DashBoard/index.tsx
@@ -1,29 +1,53 @@
 import React, { useState, useEffect } from "react";
+import { messages } from "./messages";
 import WidgetGrid from "Common/components/WidgetGrid";
-import { UserResponse } from "Data/types";
+import { UserData } from "Data/types";
 import { WidgetCardObj } from "Common/types";
+import { FormattedMessage } from "react-intl";
 
 interface Props {
-  userData?: UserResponse;
+  userData?: UserData;
 }
+
+//TODO: Default from DashBoard should of Course not be null!
 const DashBoard: React.FC<Props> = ({ userData = null }) => {
   const [widgetArray, setWidgetCardArray] = useState<Array<WidgetCardObj> | null>(null);
-  console.log(widgetArray, setWidgetCardArray);
 
   useEffect(() => {
     if (userData) {
-      let cardArray: Array<WidgetCardObj> = [];
-      console.log(cardArray);
-      setWidgetCardArray(null);
+      let cardArray: Array<WidgetCardObj> = []; //not a deep copy?
+      let card: WidgetCardObj = {
+        key: 0,
+        frontContent: (
+          <div>
+            <hr />
+            <h3>My Account</h3>
+            <p>Add more Details</p>
+          </div>
+        ),
+        backContent: (
+          <div>
+            <br></br>
+            <p>
+              <FormattedMessage {...messages.email} />: {userData.email}
+            </p>
+            <p>
+              <FormattedMessage {...messages.name} />: {userData.username}
+            </p>
+          </div>
+        ),
+        backgrColor: "white"
+      };
+      cardArray.push(card);
+      setWidgetCardArray(cardArray);
     }
-
-    // change from default to received data from Profile
   }, [userData]);
 
+  //TODO:
   // Default if no user-data is given --> can't be null / demanded by widget Array?
-  // --> need to define default on my own here.
+  // --> need to define default on my own here
 
-  return <WidgetGrid />;
+  return widgetArray ? <WidgetGrid cardArray={widgetArray} /> : null;
 };
 
 export default DashBoard;
