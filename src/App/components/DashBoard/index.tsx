1c651b65f5b7b05869fb4f16e1e75bfef92642af
Sebastian Stoff <SebastianStoff@gmx.at>
2019-08-06 18:59:42 +0200

    changed style a bit

--- src/App/components/DashBoard/index.tsx
+++ src/App/components/DashBoard/index.tsx
@@ -17,29 +17,30 @@ const DashBoard: React.FC<Props> = ({
   const WIDGET_ARRAY_DEFAULT: Array<WidgetCardObj> = [
     {
       key: 0,
-      frontContent: <AccountCard userData={userData} displayFront={true}></AccountCard>,
-      backContent: <AccountCard userData={userData} displayFront={false}></AccountCard>,
-      backgrColor: "white"
-    },
-    {
-      key: 1,
-      frontContent: <RecentActivitiesCard displayFront={true} />,
-      backContent: <RecentActivitiesCard displayFront={false} />,
-      classes: ["is-6"],
-      backgrColor: "white"
-    },
-    {
-      key: 2,
       frontContent: <SearchCard displayFront={true} />,
       backContent: <SearchCard displayFront={false} />,
       classes: ["is-6"],
       backgrColor: "white"
     },
     {
-      key: 3,
+      key: 1,
       frontContent: <AddDataCard displayFront={true} />,
       backContent: <AddDataCard displayFront={false} />,
-      classes: ["is-4"],
+      classes: ["is-6"],
+      backgrColor: "white"
+    },
+    {
+      key: 2,
+      frontContent: <AccountCard userData={userData} displayFront={true}></AccountCard>,
+      backContent: <AccountCard userData={userData} displayFront={false}></AccountCard>,
+      classes: ["is-3"],
+      backgrColor: "white"
+    },
+    {
+      key: 3,
+      frontContent: <RecentActivitiesCard displayFront={true} />,
+      backContent: <RecentActivitiesCard displayFront={false} />,
+      classes: ["is-3"],
       backgrColor: "white"
     }
   ];
