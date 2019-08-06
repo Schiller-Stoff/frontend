c552dae7de2b404cea3a5a2f5c0cef8f8fc6dede
Sebastian Stoff <SebastianStoff@gmx.at>
2019-08-06 16:13:18 +0200

    added new component for another dashboard card

---
+++ src/App/components/DashBoard/RecentActivitiesCard/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+interface Props {
+  displayFront: boolean;
+}
+
+const RecentActivitiesCard: React.FC<Props> = ({ displayFront }) => {
+  return displayFront ? (
+    <div>
+      <br></br>
+      <br></br>
+      <i className="fas fa-clipboard-list" style={{ fontSize: "7em", color: "lightgrey" }}></i>
+      <br></br>
+      <p>Recent Activities</p>
+    </div>
+  ) : (
+    <div>
+      <br></br>
+      <ul>
+        <li>Searched for: XYZ</li>
+      </ul>
+    </div>
+  );
+};
+
+export default RecentActivitiesCard;
