f4bc2f1e914ab62e3d09b05902b77731bd687a4c
Sebastian Stoff <SebastianStoff@gmx.at>
2019-08-07 16:54:20 +0200

    added base bulma modal

---
+++ src/Common/components/Modal/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Modal: React.FC = () => {
+  return (
+    <div className="modal is-active" style={{ position: "fixed", top: 0, left: 0 }}>
+      <div className="modal-background"></div>
+      <div className="modal-card">
+        <header className="modal-card-head">
+          <p className="modal-card-title">Modal title</p>
+          <button className="delete" aria-label="close"></button>
+        </header>
+        <section className="modal-card-body">Content here</section>
+        <footer className="modal-card-foot">
+          <button className="button is-success">Save changes</button>
+          <button className="button">Cancel</button>
+        </footer>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
